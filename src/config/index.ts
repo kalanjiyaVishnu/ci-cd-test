2757488eba19e34d5bdeda476e452c722aa9e3ab
kalanjiyaVishnu <kalanjiya.vishnu01@gmail.com>
2023-03-14 13:06:17 +0530

    include all configs created

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,12 @@
 import LittleSingham from "./game-configs/Little Singham.json"
+import TreasureHunt from "./game-configs/TresureHunt.json"
+import UiConfigs from "./ui-config/FinalConfig.json"
+
 const configsMap: {
   [key: string]: any
 } = {
   "little-singham": LittleSingham,
+  "treasure-hunt": TreasureHunt,
+  "ui-configs": UiConfigs,
 }
 export default configsMap
