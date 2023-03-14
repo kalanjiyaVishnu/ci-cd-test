7f718cfb09b8a1ad5e9b6aecb766ddb84492a9a5
kalanjiyaVishnu <kalanjiya.vishnu01@gmail.com>
2023-03-14 12:52:35 +0530

    include configs

---
+++ config/index.js
@@ -0,0 +1,5 @@
+import LittleSingham from `./game-configs/Little Singham.json` assert { type: `json` };
+const configsMap = {
+  "little-singham": LittleSingham,
+}
+export default configsMap
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-const express = require("express")
+import express from "express"
+import configs from "../config/"
+
 const app = express()
 const port = 3006
+
 app.get("/", (req, res) => res.send("Hello World!"))
+
+// app.get("/template/:id", (req, res) => {
+//   const { id } = req.params
+//   if (!id || !Object.keys(configs).includes(id))
+//     return res.status(400).send({ error: "Template id is invalid" })
+//   res.status(200).json(configs[id])
+// })
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
